c7fcf576faccdb60a023f13c09e0c404933ca0ee
Weiner Oliveira <woliveira82@gmail.com>
2024-02-11 22:31:42 +0000

    Section 2: Mobile Gameplay - Parallax Background

--- scripts/game.gd
+++ scripts/game.gd
@@ -6,19 +6,29 @@ var player_scene = preload("res://scenes/player.tscn")
 var camera = null
 var player: Player = null
 var player_spawn_position: Vector2
+var viewport_size: Vector2
+
 
 @onready var level_generator = $LevelGenerator
 @onready var ground_sprite = $GroundSprite
+@onready var parallax1 = $ParallaxBackground/ParallaxLayer1
+@onready var parallax2 = $ParallaxBackground/ParallaxLayer2
+@onready var parallax3 = $ParallaxBackground/ParallaxLayer3
 
 
 func _ready():
-	var viewport_size = get_viewport_rect().size
+	viewport_size = get_viewport_rect().size
 	var player_spawn_pos_y_offset = 135
 	player_spawn_position.x = viewport_size.x / 2
 	player_spawn_position.y = viewport_size.y - player_spawn_pos_y_offset
 	
 	ground_sprite.global_position.x = viewport_size.x / 2
 	ground_sprite.global_position.y = viewport_size.y
+	
+	setup_parallax_layer(parallax1)
+	setup_parallax_layer(parallax2)
+	setup_parallax_layer(parallax3)
+	
 	
 	new_game()
 
@@ -41,3 +51,19 @@ func new_game():
 
 	if player:
 		level_generator.setup(player)
+
+
+func get_parallax_sprite_scale(parallax_sprite: Sprite2D):
+	var parallax_texture = parallax_sprite.get_texture()
+	var parallax_texture_width = parallax_texture.get_width()
+	var _scale = viewport_size.x / parallax_texture_width
+	var result = Vector2(_scale, _scale)
+	return result
+
+
+func setup_parallax_layer(parallax_layer: ParallaxLayer):
+	var parallax_sprite = parallax_layer.find_child("Sprite2D")
+	if parallax_sprite:
+		parallax_sprite.scale = get_parallax_sprite_scale(parallax_sprite)
+		var my = parallax_sprite.scale.y * parallax_sprite.get_texture().get_height()
+		parallax_layer.motion_mirroring.y = my
