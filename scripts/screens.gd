9e5e59407cd7fd194b62789a87908288c5b50b0f
Weiner Oliveira <woliveira82@gmail.com>
2024-02-13 14:31:05 +0000

    Section 4: UI System - Log Messages

--- scripts/screens.gd
+++ scripts/screens.gd
@@ -1,11 +1,15 @@
 extends CanvasLayer
 
+@onready var console = $Debug/ConsoleLog
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
-	pass # Replace with function body.
+	console.visible = false
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_toggle_console_pressed():
+	console.visible = not console.visible
