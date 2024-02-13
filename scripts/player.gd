1029c7fbb6473bbdd0d8b58225fb0652b371b420
Weiner Oliveira <woliveira82@gmail.com>
2024-02-13 12:04:18 +0000

    Section 3: Exporting and Testing - Accelerometer

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,18 +2,22 @@ extends CharacterBody2D
 class_name Player
 
 var speed = 300.0
+var accelerometer_speed = 130.0
 
 var gravity = 15.0
 var max_fall_velocity = 1000
 var jump_velocity = -800
 
 var viewport_size
+var use_accelerometer = false
 
 @onready var animator = $AnimationPlayer
 
 
 func _ready():
 	viewport_size = get_viewport_rect().size
+	var os_name = OS.get_name()
+	use_accelerometer = os_name in ["Android", "iOS"]
 
 
 func _process(_delta):
@@ -29,11 +33,15 @@ func _physics_process(_delta):
 	velocity.y += gravity
 	velocity.y = min(velocity.y, max_fall_velocity)
 	
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
+	if use_accelerometer:
+		var mobile_input = Input.get_accelerometer()
+		velocity.x = mobile_input.x * accelerometer_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+		var direction = Input.get_axis("move_left", "move_right")
+		if direction:
+			velocity.x = direction * speed
+		else:
+			velocity.x = move_toward(velocity.x, 0, speed)
 	
 	move_and_slide()
 	
