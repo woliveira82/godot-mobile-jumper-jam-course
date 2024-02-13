29ab121abbe6eef3fca1234c2e2c88afa6d3f2a6
Weiner Oliveira <woliveira82@gmail.com>
2024-02-13 23:42:41 +0000

    Section 4: UI System - HUD

---
+++ scripts/hud.gd
@@ -0,0 +1,22 @@
+extends Control
+
+@onready var topbar = $TopBar
+@onready var topbar_bg = $TopBarBG
+
+
+func _ready():
+	var os_name = OS.get_name()
+	if os_name in ["Android", "iOS"]:
+		var safe_area = DisplayServer.get_display_safe_area()
+		var safe_area_top = safe_area.position.y
+		
+		if os_name == "iOS":
+			var screen_scale = DisplayServer.screen_get_scale()
+			safe_area_top /= screen_scale
+		
+		topbar.position.y += safe_area_top
+		var margin = 10
+		topbar_bg.size.y += safe_area_top + margin
+
+func _on_pause_button_pressed():
+	pass # Replace with function body.
